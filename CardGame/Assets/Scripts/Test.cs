d3ce4d05ee7ee6d553a277891a7a965ac838e512
ReynahD <wl2357@nyu.edu>
2022-06-26 00:16:15 +0900

    board stuff
    board stuff i guess

---
+++ CardGame/Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("checking if kraken is working or whatever the fuck is going on");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
